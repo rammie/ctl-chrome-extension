6ca2a8e17d56d0ba45db1ca3478dd50c24c4fcc3
Ram Mehta <ram.mehta@gmail.com>
2017-01-26 00:54:16 -0500

    Update extension binary. and add logging

--- ctl-ext/ctl-ext.js
+++ ctl-ext/ctl-ext.js
@@ -4,14 +4,17 @@
   window.addEventListener('load', WindowLoad, false);
 
   function WindowLoad(event) {
+    console.log('Loaded CTL Chrome Extension');
+
     setTimeout(() => {
+      console.log('Adding button to enlarge all windows');
       $('<button/>')
         .text('++')
         .addClass('supchat-style')
         .insertAfter('.sup-chats h1:last button:last')
         .click(() => {
+          console.log('Enlarge button clicked.');
           $('.convo-enlarge').click();
-          alert(1);
         });
 
     }, 5000);
